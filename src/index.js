15d263a0e03e6605171b9c3adb680d649a4dbd85
Eduardo Vilhalba <vilhalba.20000@gmail.com>
2021-10-29 12:41:14 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).send();
+  }
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+
+  
+  if(!user.pro && user.todos.length === 10){
+    return response.status(403).send();
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).send();
+  }
+
+  const {id} = request.params;
+
+  if(!validate(id)){
+    return response.status(400).send();
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).send();
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).send();
+  }
+
+  request.user = user;
+  return next()
+
 }
 
 app.post('/users', (request, response) => {
